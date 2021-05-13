10319d9a3401df5245c7ffdc5f57990daab33bb3
Daniel Halasz <dhalasz705@gmail.com>
2021-05-13 22:11:56 +0200

    added script for done task

--- js/script.js
+++ js/script.js
@@ -9,20 +9,41 @@
         render();
     };
 
+    const doneTask = (taskIndex) => {
+        tasks[taskIndex].done = !tasks[taskIndex].done;
+        render();
+    }
+
+    const addEvents = () => {
+        
+        const toggleDoneButtons = document.querySelectorAll(".js-done");
+
+        toggleDoneButtons.forEach((toggleDoneButtons, index) => {
+            toggleDoneButtons.addEventListener("click", () => {
+                doneTask(index);
+            });
+        });
+    }
+
     const render = () => {
         let htmlString = "";
 
         for (const task of tasks) {
             htmlString += `
             <li class="section__listItem">
-            <button class="section__listButton section__listButton--done"></button>
+            <button class="section__listButton section__listButton--done js-done">
+            ${task.done ? "✓" : ""}</button>
+
+            <span class="section__listTask${task.done ? " section__listTask--done":""}">
             ${task.content}
+            </span>
             <button class="section__listButton section__listButton--remove"></button>
             </li>
             `;
         }
 
         document.querySelector(".js-tasks").innerHTML = htmlString;
+        addEvents();
     };
 
     const onFormSubmit = (event) => {
